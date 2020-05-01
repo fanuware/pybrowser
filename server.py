740d75cd425d02a1f94a64684222c7db27a6472c
fanuware <fanuware@gmail.com>
2020-05-01 10:12:18 +0200

    Update server cli

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
+import argparse
 
-def serve_forever(port, cgi_directories):
+def serve_forever(host, port, cgi_directories):
     if sys.version_info < (3, 0):
         import CGIHTTPServer
         import BaseHTTPServer
@@ -10,20 +11,26 @@ def serve_forever(port, cgi_directories):
         class Handler(CGIHTTPServer.CGIHTTPRequestHandler):
             cgi_directories = CGI_DIRECTORIES
 
-        server = BaseHTTPServer.HTTPServer(('', port), Handler)
+        server = BaseHTTPServer.HTTPServer((host, port), Handler)
     else:
         from http.server import CGIHTTPRequestHandler, HTTPServer
 
         handler = CGIHTTPRequestHandler
         handler.cgi_directories = CGI_DIRECTORIES
 
-        server = HTTPServer(('localhost', port), handler)
+        server = HTTPServer((host, port), handler)
 
-    print ('Server started on port', port)
+    print ('Server started ({}, {})'.format(host, port))
     server.serve_forever()
 
 
 if __name__ == '__main__':
-    PORT = 8440 if len(sys.argv) <= 1 else int(sys.argv[1])
+    parser = argparse.ArgumentParser(description='Run CGI Server.')
+    parser.add_argument('--host', default='localhost',
+                        help='Server host address')
+    parser.add_argument('--port', type=int, default=8440,
+                        help='Server port')
+    args = parser.parse_args()
+
     CGI_DIRECTORIES = ['/cgi-bin']
-    serve_forever(PORT, CGI_DIRECTORIES)
+    serve_forever(args.host, args.port, CGI_DIRECTORIES)
